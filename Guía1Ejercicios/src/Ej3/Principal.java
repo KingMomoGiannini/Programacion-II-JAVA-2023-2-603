bb88770974d183a0ff734b638300f3d38723cd1a
Sebastian Giannini <sebastiangiannini95@gmail.com>
2023-04-17 17:36:37 -0300

    Ejercicio 8

--- Guía1Ejercicios/src/Ej3/Principal.java
+++ Guía1Ejercicios/src/Ej3/Principal.java
@@ -21,66 +21,7 @@ public class Principal {
         p1.getSaldo();
         p1.setExtraccion(500);
         p1.getUltimosCBU(3);
-        p1.getDatos();    }
-    
-<<<<<<< HEAD
+        p1.getDatos();    
+    }
 }
-=======
-}/*
-private String CBU;
-    private String tipoCaja;
-    private double saldo;
-
-    public CuentaBancaria(String tipoCaja) {
-        this.tipoCaja = tipoCaja;
-        this.CBU = generarCBU();
-    }
-
-    public void getDatos(){
-        System.out.println(toString());
-    }
-    
-    public void getSaldo() {
-        System.out.println("Saldo: $" + saldo);
-    }
-    
-    public void setDepositar(double monto){
-        if(monto>0){
-            saldo += monto;
-        }
-        else{
-            System.out.println("Error de depósito");
-        }
-    }
-    
-    public void setExtraccion(double monto){
-        if(tipoCaja.equals("Cuenta corriente") || saldo>=monto ){
-            saldo -=monto;
-            System.out.println("Se han extraido: $" + monto);
-        }
-        else{
-            System.out.println("Saldo insuficiente para el tipo de cuenta que posee.");
-        }
-    }
-    
-    private String generarCBU() {
-        Random r = new Random();
-        String elCbu = "";
-        for (int i = 0; i < 22; i++) {
-            elCbu += r.nextInt(10);
-        }
-        return elCbu;        
-    }
-    
-    public void getUltimosCBU(int cuantos){
-        String ultimos = CBU.substring(CBU.length() - cuantos);
-        System.out.println("Últimos " + cuantos + " dígitos del CBU: " + ultimos);
-    }
-
-    @Override
-    public String toString() {
-        return "CuentaBancaria{" + "CBU=" + CBU + ", tipoCaja=" + tipoCaja + ", saldo=" + saldo + '}';
-    }
-
-*/
->>>>>>> 1e7e90b (Ejercicio 8)
+   
--- Guía1Ejercicios/src/Ej8/Principal.java
+++ Guía1Ejercicios/src/Ej8/Principal.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -55,61 +54,3 @@ public class Principal {
     }
     
 }
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package Ej8;
-/**
-
-8) Desarrollá la clase Password, que permita tener contraseñas como objetos. Su único atributo 
-será el valor de la password (String). Debe responder a los siguientes métodos:
-
-▪ boolean esFuerte() {…}
-Devuelve si la password es fuerte o no. Una password es fuerte cuando tenga al menos 8
-caracteres.
-
-▪ boolean nuevoValor(String) {…}
-Establece como nuevo valor de password el recibido como parámetro, siempre y cuando su 
-longitud sea mayor o igual a 6, si no, lo deja como estaba. Devuelve si se pudo o no establecer 
-el valor
-
-▪ String generarAleatorio(int) {…}
-Devuelve una cadena que representa un valor de password aleatorio cuya longitud coincida 
-con el parámetro recibido. Si el parámetro es menor que 6, devuelve null.
-
-Además, deben poder crearse passwords con o sin valor inicial, por ello es que la clase contará con 
-un constructor sobrecargado:
-▪ Password(String) {…}
-Crea un password cuyo valor viene dado por parámetro.
-▪ Password() {…}
-Crea un password cuyo valor se crea automáticamente.
-
- * @author giann
- */
-public class Principal {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        Password p = new Password("73i32");
-        Password p1= new Password();
-        p.getPassword();
-        p.esFuerte();
-        p.nuevoValor("5673ui32");
-        p.getPassword();
-        p.esFuerte();
-        p.nuevoValor("983791784");
-        p.getPassword();
-        p.esFuerte();  
-        p.nuevoValor(p.generarAleatorio(10));
-        p.getPassword();
-        p.esFuerte();
-        p1.getPassword();
-    }
-    
-}
->>>>>>> 1e7e90b (Ejercicio 8)
